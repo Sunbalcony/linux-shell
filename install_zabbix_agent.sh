3289e6943458da3e9faa053654091a763ffa365f
Sunbalcony <1141789386@qq.com>
2019-11-25 22:03:59 +0800

    Update install_zabbix_agent.sh

--- install_zabbix_agent.sh
+++ install_zabbix_agent.sh
@@ -1,4 +1,4 @@
-# #!/usr/bin/env bash
+#!/usr/bin/env bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 ZBXP=$(netstat -ntlp |grep tcp |grep -v tcp6| grep 10050|awk '{print $4}'|awk -F : '{print $2}')
@@ -30,31 +30,13 @@ function install_agent(){
 check_process(){
     echo "正在启动Zabbix-Agent"
     systemctl start zabbix-agent
-#     sleep 8
-#     if [[ ${ZBXP} == 10050 ]]
-#     then
-#             echo -e "zabbix-agent启动成功"
-#     else
-#             echo -e "zabbix-agent启动失败，请检查"
-#     fi
-# } 不知为何总是检查失败 先屏蔽
-function check_firewall(){
-	if [ -e "/etc/sysconfig/iptables" ]
-	then
-		iptables -I INPUT -p tcp --dport 10050 -j ACCEPT
-		service iptables save
-		service iptables restart
-		echo "##############################iptables配置完成##############################"
-	elif [ -e "/etc/firewalld/zones/public.xml" ]
-	then
-		firewall-cmd --zone=public --add-port=10050/tcp --permanent
-		firewall-cmd --reload
-		echo "##############################firewall配置完成##############################"
-	elif [ -e "/etc/ufw/before.rules" ]
-	then
-		sudo ufw allow 10050/tcp
-		echo "##############################ufw配置完成##############################"
-	fi
+    sleep 8
+    if [[ ${ZBXP} == 10050 ]]
+    then
+            echo -e "zabbix-agent启动成功"
+    else
+            echo -e "zabbix-agent启动失败，请检查"
+    fi
 }
 echo "Centos7 Zabbix-Agent 一键安装脚本 by Sunbalcony"
 echo "1) 安装Zabbix-Agent"
@@ -65,8 +47,7 @@ case $emmm in
             check_env
             check_env && \
             install_agent && \
-            check_process && \
-	    check_firewall
+            check_process
 	;;
         2)
             systemctl restart zabbix-agent
